e0d0c11ff85127c9fe84ae50a0d702d8df467fe1
cwiza <bhip_caker@hotmail.com>
2025-07-18 13:12:09 -0700

    Add React app structure and build configuration

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Indy Radio</h1>
+        <p>Supporting publicly funded radio stations</p>
+      </header>
+      <main>
+        <div className="content">
+          <p>Welcome to Indy Radio!</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
